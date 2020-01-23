038e979d37b12b938ce1e5c7a02a157474b7af79
Charles Pelton <charliejp0311@gmail.com>
2020-01-23 16:06:59 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(a) # put argument(s) here
   # code here
+  i = 0
+  while i < a.length 
+    a.collect do |in_a|
+      puts a[i]
+      i += 1
+    end
+  end
 end
